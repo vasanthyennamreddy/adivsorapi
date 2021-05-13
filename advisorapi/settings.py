ad3b9350d973d038738aa20838cea7848f23d679
vasanth_yennamreddy <yvr10@iitbbs.ac.in>
2021-05-14 02:34:37 +0530

    modified settings again

--- advisorapi/settings.py
+++ advisorapi/settings.py
@@ -149,3 +149,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
